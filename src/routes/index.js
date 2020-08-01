536c52b1c8e030b0c951ff92559dc27eecc05df8
Neha Yadav <nehayadav0102@gmail.com>
2020-08-01 21:05:20 +0530

    added homepage message

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,12 @@ function init(app) {
     routes.forEach(route => {
         const routePath = path.resolve(__dirname, `./${route.fileName}`);
         app.use(route.mount, require(routePath));
+    });
+    app.use('/', (req, res) => {
+        res.write('<h1>Welcome to Portfolio Tracking API<h1>');
+        res.write('<h2>Available routes and all the documentaion can be found at:<h2>');
+        res.write('<a href="https://github.com/neha01/Portfolio-Tracking-API"> Github - Portfolio Tracking API</a>');
+        res.end();
     })
 }
 
